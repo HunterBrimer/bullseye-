911831a94fab9f837ae0fc083f12e9e572b5e564
hunter <hunter@James-MacBook-Pro-USB-C.local>
2019-07-23 23:19:51 -0700

    committing all files

---
+++ Bulls Eye/AboutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutViewController.swift
+//  Bulls Eye
+//
+//  Created by hunter on 7/23/19.
+//  Copyright © 2019 hunter. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score = 0
+    var round = 1
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider?.value.rounded()
+        currentValue = Int(roundedValue!)
+        startNewGame()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider?.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider?.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider?.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SmallButton")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider?.setMaximumTrackImage(trackRightResizable, for: .normal)    }
+    
+    @IBAction func showAlert() {
+        
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Nice!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default, handler: {
+            action in
+            self.startNewRound()
+        })
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+        
     }
-
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedvalue = slider.value.rounded()
+        currentValue = Int(roundedvalue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider?.value = Float(currentValue)
+        round = round + 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+        
+        
+    }
+    
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
 }
 
